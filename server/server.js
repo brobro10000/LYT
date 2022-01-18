b5135b09f58b68e5b2c3a7cf1ca12f26725ae35b
Hamzah <82611798+brobro10000@users.noreply.github.com>
2022-01-18 01:42:57 -0500

    Matched Ticked off

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
@@ -28,12 +29,13 @@ start();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
 }
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
-db.once('open', () => { 
+
+db.once("open", () => {
   // integrate our Apollo server with the Express application as middleware
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
