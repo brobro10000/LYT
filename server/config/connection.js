f143346b88566e6df24b68d23cc02900973ba3fd
Hamzah <82611798+brobro10000@users.noreply.github.com>
2022-01-18 01:24:04 -0500

    server stuff

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lyte");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lyte", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -33,10 +33,11 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-
+db.once('open', () => { 
   // integrate our Apollo server with the Express application as middleware
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
+});
